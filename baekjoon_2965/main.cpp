85a657cbe70c935ff03dba2acbfdafc6b27ed0b0
WaniFi <skelpd24@gmail.com>
2018-08-18 16:25:06 +0900

    2965 문제 더 간단한 풀이 기록

--- baekjoon_2965/main.cpp
+++ baekjoon_2965/main.cpp
@@ -18,3 +18,12 @@ int main() {
 	cout << r;
 	return 0;
 }
+/*
+가장 긴 범위만큼 많이 이동할 수 있기 때문에
+이와 같은 간단한 풀이가 가능하다.
+int main(){
+	cin >> a >> b >> c;
+	cout << ((c-b > b-a) ? c-b-1 : b-a-1);
+	return 0;
+}
+*/
